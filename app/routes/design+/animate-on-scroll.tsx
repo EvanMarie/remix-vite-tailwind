2894c7c1f29dedad411b592eb5b125124eb9ca0a
Evan Marie Carr <evanmariecarr@gmail.com>
2024-02-22 14:15:58 -0500

    animate on scroll with framer motion

--- app/routes/design+/animate-on-scroll.tsx
+++ app/routes/design+/animate-on-scroll.tsx
@@ -1,96 +1,52 @@
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
 import Flex from "~/components/buildingBlocks/flex";
-import FlexFull from "~/components/buildingBlocks/flexFull";
-import Heading from "~/components/buildingBlocks/headingText";
-import Text from "~/components/buildingBlocks/text";
-import VStack from "~/components/buildingBlocks/vStack";
 import VStackFull from "~/components/buildingBlocks/vStackFull";
-import "~/animate-on-scroll/animate-on-scroll.css";
-
-function FullPage({
-  children,
-  className,
-  aos,
-}: {
-  children: React.ReactNode;
-  className?: string;
-  aos?: string;
-}) {
-  {
-    return (
-      <div
-        data-aos={aos}
-        className={`flex w-[100vw] h-[100vh] min-h-[100vh] justify-center items-center ${className}`}
-      >
-        {children}
-      </div>
-    );
-  }
-}
+import AnimatedComponent from "~/components/animate-on-scroll/animateOnScrollOne";
 
 function SingleSection({
   height = "h-[40vh]",
-  bg = "bg-col-800",
-  aos = "fade-right",
-  text,
+  bg = "bg-red-200",
+  textColor = "text-black",
+  children,
 }: {
   height?: string;
   bg?: string;
-  aos?: string;
-  text?: string;
+  textColor?: string;
+  children?: React.ReactNode;
 }) {
   return (
     <div
-      className={`w-screen ${height} ${bg} flex justify-center items-center`}
+      className={`w-screen ${height} ${bg} ${textColor} flex justify-center items-center`}
     >
-      <div data-aos={aos}>
-        <Heading text={text} layout="text-insane-looser" className="p-[4vh]" />
-      </div>
+      {children}
     </div>
   );
 }
 
 export default function AnimateOnScroll() {
-  useEffect(() => {
-    AOS.init();
-    AOS.refresh();
-  }, []);
-
   return (
     <Flex className="w-screen h-screen overflow-y-auto">
-      <VStack className="h-fit">
-        <div data-aos="fade-right" className="text-[20vh]">
-          One
-        </div>
-        <div data-aos="fade-right" className="text-[20vh]">
-          Two
-        </div>
-        <div data-aos="fade-right" className="text-[20vh]">
-          Three
-        </div>
-        <div data-aos="fade-right" className="text-[20vh]">
-          Four
-        </div>
-        <div data-aos="fade-right" className="text-[20vh]">
-          Five
-        </div>
-        <div data-aos="fade-right" className="text-[20vh]">
-          Six
-        </div>
-        <div data-aos="fade-right" className="text-[20vh]">
-          Seven
-        </div>
-      </VStack>
-
-      {/* <SingleSection text="One" aos="fade-right" />
-        <SingleSection text="Two" aos="fade-right" />
-        <SingleSection text="Three" aos="fade-right" />
-        <SingleSection text="Four" aos="fade-right" />
-        <SingleSection text="Five" aos="fade-right" />
-        <SingleSection text="Six" aos="fade-right" />
-        <SingleSection text="Seven" aos="fade-right" /> */}
+      <VStackFull className="h-fit p-[1.5vh]">
+        <SingleSection bg="bg-blue-200">
+          <AnimatedComponent>
+            <h1 className="text-3xl font-bold underline">Animate me!</h1>
+          </AnimatedComponent>
+        </SingleSection>
+        <SingleSection bg="bg-purple-200">
+          <AnimatedComponent>
+            <h1 className="text-3xl font-bold underline">Animate me!</h1>
+          </AnimatedComponent>
+        </SingleSection>
+        <SingleSection bg="bg-green-200">
+          <AnimatedComponent>
+            <h1 className="text-3xl font-bold underline">Animate me!</h1>
+          </AnimatedComponent>
+        </SingleSection>
+        <SingleSection bg="bg-yellow-200">
+          <AnimatedComponent>
+            <h1 className="text-3xl font-bold underline">Animate me!</h1>
+          </AnimatedComponent>
+        </SingleSection>
+      </VStackFull>
     </Flex>
   );
 }
