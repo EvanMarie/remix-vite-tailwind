39f5dbbab502ce700cb0ce238a108b0cabbcaaa6
Evan Marie Carr <evanmariecarr@gmail.com>
2024-03-02 14:18:46 -0500

    wrong repo! Oops

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -17,15 +17,18 @@ export default function Index() {
       <Transition className="w-full h-full justify-center items-center">
         <VStack>
           <Text className="font-cursive boldTextGlow text-col-900 text-stroke-8-170 text-[10vh] ">
-            Aceternity Animation
+            Remix, Vite, & Tailwind
           </Text>
           <Text className="text-col-100 text-2xl textShadow">
-            Fun with animations!
+            A Project Launchpad with Vite, Tailwind CSS, and Remix Flat Routes
           </Text>
-
+          <Text className="text-col-100 text-2xl textShadow">
+            including extensive preset options and components with quick
+            customization.
+          </Text>
           <NavLink to="/design">
             <Text className="p-[1.5vh] bg-100-linear3op25 text-col-900 shadowBroadNormal hover:bg-400-diagonal3op75 transition-400">
-              Design Options
+              Preset Design Options
             </Text>
           </NavLink>
         </VStack>
