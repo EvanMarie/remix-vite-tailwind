600ef1a8f03e5973cf15f92c3e31d3bf6745c0b1
Evan Marie Carr <evanmariecarr@gmail.com>
2024-03-24 15:29:23 -0400

    toggle switch

---
+++ app/components/buildingBlocks/toggleSwitch.tsx
@@ -0,0 +1,154 @@
+import { useState } from "react";
+import Flex from "./flex";
+import HStack from "./hStack";
+import VStack from "./vStack";
+import Text from "./text";
+import { TbCross, TbCrossOff } from "react-icons/tb";
+import Icon from "./icon";
+
+export default function ToggleSwitch({
+  size = "lg",
+  bgColor = "bg-col-840",
+  switchColor = "bg-col-100",
+  switchShadow = "dark",
+  containerShadow = "dark",
+  onIcon = TbCross,
+  offIcon = TbCrossOff,
+  onText,
+  offText,
+  labelColor = "dark",
+}: {
+  size: "xs" | "sm" | "md" | "lg" | "xl";
+  bgColor: string;
+  switchColor: string;
+  switchShadow: "dark" | "light" | "none";
+  containerShadow: "dark" | "light" | "none";
+  onIcon?: React.ComponentType<{ className?: string }>;
+  offIcon?: React.ComponentType<{ className?: string }>;
+  onText?: string;
+  offText?: string;
+  labelColor: "dark" | "light" | "none";
+}) {
+  const switchContainerSizes = {
+    xs: `w-[5.4vh] h-[2.6vh] rounded-[1.1vh] px-[0.4vh] ${
+      containerShadow === "dark"
+        ? "shadowNarrowTight border-970-sm"
+        : "lightShadow border-170-sm"
+    }
+    `,
+    sm: `w-[6.1vh] h-[3vh] rounded-[2vh] px-[0.4vh] ${
+      containerShadow === "dark"
+        ? "shadowNarrowTight border-970-sm"
+        : "lightShadow border-170-sm"
+    }
+    `,
+    md: `w-[6.4vh] h-[3vh] rounded-[2vh] px-[0.4vh] ${
+      containerShadow === "dark"
+        ? "shadowBroadTight border-970-sm"
+        : "lightShadow border-170-md"
+    }
+    `,
+    lg: `w-[6.5vh] h-[3.5vh] rounded-[4vh] px-[0.4vh] ${
+      containerShadow === "dark"
+        ? "shadowBroadNormal border-970-md"
+        : "lightShadow border-170-md"
+    }
+    `,
+    xl: `w-[7.2vh] h-[3.8vh] rounded-[3.7vh] px-[0.4vh] ${
+      containerShadow === "dark"
+        ? "shadowBroadNormal border-970-md"
+        : "lightShadow border-170-md"
+    }
+    `,
+  };
+
+  const switchSizes = {
+    xs: `w-[2vh] h-[1.7vh] rounded-[1vh] ${
+      switchShadow === "dark"
+        ? "shadowNarrowTight border-970-sm"
+        : "lightShadow border-170-sm"
+    }`,
+    sm: `w-[2.3vh] h-[2vh] rounded-full border-990-sm ${
+      switchShadow === "dark"
+        ? "shadowNarrowTight border-970-sm"
+        : "lightShadow border-170-sm"
+    }`,
+    md: `w-[2.6vh] h-[2.3vh] rounded-full border-990-md ${
+      switchShadow === "dark"
+        ? "shadowBroadTight border-980-md"
+        : "lightShadow border-170-sm"
+    }`,
+    lg: `w-[2.9vh] h-[2.5vh] rounded-full border-990-md ${
+      switchShadow === "dark"
+        ? "shadowBroadTight border-980-md"
+        : "lightShadow border-170-sm"
+    }`,
+    xl: `w-[3.3vh] h-[2.8vh] rounded-full border-990-md ${
+      switchShadow === "dark"
+        ? "shadowBroadTight border-980-nd"
+        : "lightShadow border-170-sm"
+    }`,
+  };
+
+  const labelSizes = {
+    xs: "text-[1.8vh]",
+    sm: "text-[2vh]",
+    md: "text-[2.2vh]",
+    lg: "text-[2.4vh]",
+    xl: "text-[2.6vh]",
+  };
+
+  const gap = {
+    xs: "gap-[0.5vh]",
+    sm: "gap-[0.6vh]",
+    md: "gap-[0.7vh]",
+    lg: "gap-[0.8vh]",
+    xl: "gap-[0.9vh]",
+  };
+
+  const [switchOn, setSwitchOn] = useState(false);
+  return (
+    <VStack>
+      <Text>Switch Status:</Text>
+      <Text>{switchOn ? "ON" : "OFF"}</Text>
+      <HStack className="items-center " gap={gap[size]}>
+        <Flex
+          className={`h-full items-center ${labelSizes[size]} ${
+            labelColor === "dark"
+              ? "text-col-100 bg-col-950"
+              : "text-col-900 bg-col-150"
+          } p-[0.3vh] ${switchOn ? "opacity-60" : "opacity-1"}`}
+        >
+          {onIcon ? (
+            <Icon icon={onIcon} iconClassName={labelSizes[size]} />
+          ) : onText ? (
+            <Text className={labelSizes[size]}>{onText}</Text>
+          ) : undefined}{" "}
+        </Flex>
+        <HStack
+          className={`${switchContainerSizes[size]} ${
+            switchOn ? "justify-start" : "justify-end"
+          } items-center ${bgColor} cursor-pointer`}
+          onClick={() => setSwitchOn(!switchOn)}
+        >
+          <Flex
+            className={`${switchSizes[size]} ${switchColor} shadowNarrowTight`}
+          />
+        </HStack>{" "}
+        <Flex
+          className={`h-full items-center ${
+            labelColor === "dark"
+              ? "text-col-100 bg-col-950"
+              : "text-col-900 bg-col-150"
+          } p-[0.3vh]  ${!switchOn ? "opacity-60" : "opacity-1"}`}
+        >
+          {offIcon ? (
+            <Icon icon={offIcon} iconClassName={labelSizes[size]} />
+          ) : offText ? (
+            <Text className={labelSizes[size]}>{offText}</Text>
+          ) : undefined}
+        </Flex>
+      </HStack>
+    </VStack>
+  );
+}
--- app/routes/design+/test.tsx
+++ app/routes/design+/test.tsx
@@ -1,5 +1,13 @@
-import AnimatedParallaxImage from "~/components/specialty/animatedParallaxImage";
+import Center from "~/components/buildingBlocks/center";
+import CenterFull from "~/components/buildingBlocks/centerFull";
+import ToggleSwitch from "~/components/buildingBlocks/toggleSwitch";
 
 export default function Test() {
-  return <AnimatedParallaxImage />;
+  return (
+    <CenterFull>
+      <Center className="w-[30vh] h-[20vh] bg-col-140">
+        <ToggleSwitch />
+      </Center>
+    </CenterFull>
+  );
 }
