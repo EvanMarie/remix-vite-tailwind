2ced4d251047de88bffe05ec730cb01e8ee46b42
Evan Marie Carr <evanmariecarr@gmail.com>
2024-04-03 08:54:55 -0400

    mothra

--- app/routes/design+/demos+/ramen-bar+/index.tsx
+++ app/routes/design+/demos+/ramen-bar+/index.tsx
@@ -1,15 +1,18 @@
 import LayoutContainer from "~/components/buildingBlocks/layoutContainer";
 import DemoNav from "../components/demoNav";
 import TransitionFull from "~/components/buildingBlocks/TransitionFull";
+import ParallaxShiftingImage from "~/components/specialty/parallaxShiftingImage";
+import CenterFull from "~/components/buildingBlocks/centerFull";
 
 export default function RamenBarInded() {
   return (
-    <TransitionFull className="relative">
-      {" "}
-      <DemoNav />
-      <LayoutContainer>
-        <h1>Ramen Bar</h1>
-      </LayoutContainer>
-    </TransitionFull>
+    <LayoutContainer className="bg-[url('/images/ramenBack.png')]">
+      <TransitionFull className="relative">
+        <DemoNav />
+        <CenterFull>
+          <ParallaxShiftingImage bgImageLink="/images/moth.png" />
+        </CenterFull>
+      </TransitionFull>
+    </LayoutContainer>
   );
 }
