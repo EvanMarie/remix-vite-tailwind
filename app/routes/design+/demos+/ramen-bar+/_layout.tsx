19b659a26170fc329d9e704a5b1065eec482fac5
Evan Marie Carr <evanmariecarr@gmail.com>
2024-04-04 14:20:10 -0400

    added layout to ramen

--- app/routes/design+/demos+/ramen-bar+/_layout.tsx
+++ app/routes/design+/demos+/ramen-bar+/_layout.tsx
@@ -1,9 +1,9 @@
 import { Outlet } from "@remix-run/react";
 import LayoutContainer from "~/components/buildingBlocks/layoutContainer";
 
-export default function RouteIndex() {
+export default function RamenLayout() {
   return (
-    <LayoutContainer>
+    <LayoutContainer className="bg-[url('/images/demo_images/moth_ramen_tokyo.png')] bg-center bg-cover ">
       <Outlet />
     </LayoutContainer>
   );
